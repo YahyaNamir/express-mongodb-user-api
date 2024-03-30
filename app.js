6eae07baba354b6a7feaacb19b3f03d6407cebe0
Yahya Namir <yahyanamir5@gmail.com>
2024-03-30 10:10:40 +0000

    app2

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const User = require("./user");
 const app = express();
 app.use(express.json());
 
-// app.get("/users", async (req, res) => {
-//   try {
-//     const users = await User.find();
-//     res.json(users);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // app.get("/users/:id", getUser, (req, res) => {
 //   res.json(res.user);
